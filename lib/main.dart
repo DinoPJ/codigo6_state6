e222e2fa219fe5b4eda20041af769e49b721e92d
DinoPJ <dinopanta.1309@gmail.com>
2023-07-24 01:30:34 -0500

    agregamos boton y cambiamos a un statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 100;
+  String nombre = "Dino Panta";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            counter.toString(),
+            style: TextStyle(
+              fontSize: 40.0,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              counter = counter + 10;
+              setState(
+                  () {}); //redibuja todo desde el  Widget build(BuildContext context) {
+
+              print(counter);
+            },
+            child: Text("Click"),
+          ),
+        ]),
+      ),
+    );
   }
 }
