46312c57bdec0b2d1ff7caae6c6f1562ae355801
DinoPJ <dinopanta.1309@gmail.com>
2023-07-24 00:05:41 -0500

    Agrego un Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
